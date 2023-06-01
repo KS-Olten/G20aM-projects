84c8a64d59604969010dd72ab3b8d40bc5927dbd
JohannesHool <hooljohannes@gmail.com>
2023-06-01 10:31:22 +0200

    rename main script

--- home.py
+++ home.py
@@ -4,3 +4,4 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 st.header('Informatikprojekte G20aM')
+st.image('https://upload.wikimedia.org/wikipedia/commons/0/02/Computer_science_education.png', width=800)
